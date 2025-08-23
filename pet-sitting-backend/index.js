25ef98c18b2368cb60252b1fd9d7e97e3ce6699b
jelenap19 <jelena.pasagic12@gmail.com>
2025-08-23 20:58:03 +0200

    backend deployment fix

--- pet-sitting-backend/index.js
+++ pet-sitting-backend/index.js
@@ -1,7 +1,7 @@
 import app from "./server.js";
 import "dotenv/config.js";
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 7000;
 
 app.listen(PORT, () => {
   console.log(`App listening on http://localhost:${PORT}`);
